e9e121b1d6e89d948d92de2f0d244bc50f146f58
bstefansen <bstefansen11@gmail.com>
2019-11-29 12:07:06 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Loan Interest Calculator
+
+A simple calculator for loan interest
+
+Allows the input of "K" to signify "1,000" in the loan amount.
--- test.py
+++
@@ -1,9 +0,0 @@
-from decimal import *
-getcontext().prec = 8
-
-print("")
-num = '${:,.2f}'.format(float(input("Enter a number: ")))
-print("")
-
-print(num)
-print("")
